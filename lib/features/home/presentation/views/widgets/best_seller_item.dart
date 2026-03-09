46642a6633b781b837362dc0d8d32fa7ad83d0f8
Ziad Tamer <ztamer494@gmail.com>
2026-03-09 11:49:49 +0200

    best seller item part 3

--- lib/features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -26,14 +27,30 @@ class BestSellerItem extends StatelessWidget {
             ),
           ),
           Column(
+            spacing: 3,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * .5,
                 child: Text(
                   'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
                 ),
+              ),
+              Text('J.K Rowling', style: Styles.textStyle14),
+              Row(
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 50),
+                  Text('⭐ 4.8'),
+                  SizedBox(width: 5),
+                  Text('(2390)'),
+                ],
               ),
             ],
           ),
