0718daa23f42c0952b7b563e7473b15651494507
Ziad Tamer <ztamer494@gmail.com>
2026-03-10 00:33:09 +0200

    rename app router file

--- lib/features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/ap_router.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
