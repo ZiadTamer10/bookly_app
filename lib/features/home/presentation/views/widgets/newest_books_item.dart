e95d36efc30b1c24ea2ab9c398f6febe4e5a8d5e
Ziad Tamer <ztamer494@gmail.com>
2026-03-16 15:18:21 +0200

    fetch similar books done

--- lib/features/home/presentation/views/widgets/newest_books_item.dart
+++ lib/features/home/presentation/views/widgets/newest_books_item.dart
@@ -14,7 +14,7 @@ class NewestBooksItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+        GoRouter.of(context).push(AppRouter.kBookDetailsView, extra: bookModel);
       },
       child: SizedBox(
         height: 135,
--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,24 +1,39 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_error_widget.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_loading_indicator.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(right: 5),
-            child: FeaturedItem(
-              imageUrl: 'https://pbs.twimg.com/media/GQ7yv4_XwAAsBCY.jpg',
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .15,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: FeaturedItem(
+                    imageUrl: 'https://pbs.twimg.com/media/GQ7yv4_XwAAsBCY.jpg',
+                  ),
+                );
+              },
             ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
