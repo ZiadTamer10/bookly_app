e0fc2a983b0d456d74bfcd9e7b049d3f11883ac5
Ziad Tamer <ztamer494@gmail.com>
2026-03-17 16:25:33 +0200

    Bookly App Done

--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,5 +1,4 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_error_widget.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_loading_indicator.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/newest_books_item.dart';
 import 'package:bookly_app/features/search/presentation/manager/search_books_cubit/search_books_cubit.dart';
 import 'package:flutter/material.dart';
@@ -26,7 +25,7 @@ class SearchResultListView extends StatelessWidget {
         } else if (state is SearchBooksFailure) {
           return CustomErrorWidget(errMessage: state.errMessage);
         } else {
-          return CustomLoadingIndicator();
+          return Text('');
         }
       },
     );
